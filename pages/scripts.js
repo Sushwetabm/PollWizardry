8865255ac1c81882d40af4ce7543e656eddebcfc
TanyaSingh103 <tanya.103.singh@gmail.com>
2023-11-29 12:30:24 +0530

    changed the form_page

--- pages/scripts.js
+++ pages/scripts.js
@@ -1,29 +1,38 @@
-$(document).ready(function () {
-    $("#input-type").change(function () {
-        var inputType = $(this).val();
-        if (inputType === "dropdown") {
-            $("#dropdown-options").show();
-        } else {
-            $("#dropdown-options").hide();
-        }
-    });
+function addQuestion() {
+    const questionNumber = document.getElementById('questions').childElementCount + 1;
+    const questionDiv = document.createElement('div');
+    questionDiv.innerHTML = `
+        <h3>Question ${questionNumber}</h3>
+        <label for="questionText${questionNumber}">Question Text:</label>
+        <input type="text" id="questionText${questionNumber}" name="questionText${questionNumber}" required>
+        <br>
+        <label for="option${questionNumber}">Options:</label>
+        <input type="text" id="option${questionNumber}" name="option${questionNumber}" required>
+        <button type="button" onclick="addOption(this)">Add Option</button>
+        <br>
+        <label for="correctOption${questionNumber}">Correct Option:</label>
+        <select id="correctOption${questionNumber}" name="correctOption${questionNumber}" required>
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+            <option value="option4">Option 4</option>
+        </select>
+    `;
+    document.getElementById('questions').appendChild(questionDiv);
+}
 
-    $("#form-builder").submit(function (event) {
-        event.preventDefault();
-        var inputType = $("#input-type").val();
-        var inputLabel = $("#input-label").val();
-        var inputName = $("#input-name").val();
+function addOption(element) {
+    const optionNumber = element.parentElement.getElementsByTagName('input').length + 1;
+    const optionInput = document.createElement('input');
+    optionInput.type = 'text';
+    optionInput.id = `option${optionNumber}`;
+    optionInput.name = `option${optionNumber}`;
+    optionInput.required = true;
+    element.parentElement.appendChild(optionInput);
 
-        var inputHtml = '';
-        if (inputType === "dropdown") {
-            var option1 = $("#option1").val();
-            var option2 = $("#option2").val();
-            inputHtml = `<label for="${inputName}">${inputLabel}</label><select name="${inputName}" id="${inputName}"><option value="${option1}">${option1}</option><option value="${option2}">${option2}</option></select>`;
-        } else {
-            inputHtml = `<label for="${inputName}">${inputLabel}</label><input type="${inputType}" name="${inputName}" id="${inputName}" required>`;
-        }
-
-        $("#dynamic-form").append(inputHtml);
-        $(this).trigger("reset");
-    });
-});
+    const correctOptionSelect = element.parentElement.getElementsByTagName('select')[0];
+    const newOption = document.createElement('option');
+    newOption.value = `option${optionNumber}`;
+    newOption.text = `Option ${optionNumber}`;
+    correctOptionSelect.appendChild(newOption);
+}
