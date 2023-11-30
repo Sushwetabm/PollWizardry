f6c1851ef63628edc21ac551016a2d38fa4aec6f
TanyaSingh103 <tanya.103.singh@gmail.com>
2023-11-30 13:53:29 +0530

    added css to submit pages

--- pages/jobsatisfaction_submit.php
+++ pages/jobsatisfaction_submit.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job Satisfaction Survey</title>
+    <!-- Add Chart.js library -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
         body {
@@ -39,7 +40,7 @@
 
         canvas {
             max-width: 80%;
-            max-height: 180px;
+            max-height: 200px;
             width: auto;
             height: auto;
             display: block;
@@ -50,29 +51,31 @@
 <body>
 
 <?php
-
+// Replace these variables with your actual database connection details
 $servername = "localhost";
 $username = "root";
-$password = "Sush#2004";
+$password = "";
 $dbname = "pollwizardry";
 $tableQ = "jobsatisfactionq";
 $tableA = "jobsatisfactiona";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+    // Loop through each question
     foreach ($_POST as $key => $value) {
-        
+        // Check if the field is a question field
         if (substr($key, 0, 1) == 'q') {
-            $qid = substr($key, 1); 
+            $qid = substr($key, 1); // Extract QID from the field name
 
-            
+            // Update the corresponding row in the JobSatisfactionA table
             $sql = "UPDATE $tableA SET Option1 = Option1 + " . ($value == 'A' ? 1 : 0) . ",
                                         Option2 = Option2 + " . ($value == 'B' ? 1 : 0) . ",
                                         Option3 = Option3 + " . ($value == 'C' ? 1 : 0) . ",
@@ -86,20 +89,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Survey data successfully submitted!";
 }
 
-
+// Fetch questions and options from JobSatisfactionQ
 $sql = "SELECT * FROM $tableQ";
 $result = $conn->query($sql);
 
-
+// Check if any data is available
 if ($result->num_rows > 0) {
-<<<<<<< Updated upstream
-    
-=======
     // Open the table
     echo "<center><table>";
 
     // Display data in tables and pie charts
->>>>>>> Stashed changes
     while ($row = $result->fetch_assoc()) {
         $qid = $row['QID'];
         $question = $row['Question'];
@@ -108,32 +107,20 @@ if ($result->num_rows > 0) {
         $option3 = $row['Option3'];
         $option4 = $row['Option4'];
 
-        
+        // Fetch count of each option from JobSatisfactionA
         $countSql = "SELECT Option1, Option2, Option3, Option4 FROM $tableA WHERE QID = $qid";
         $countResult = $conn->query($countSql);
 
         if ($countResult->num_rows > 0) {
-<<<<<<< Updated upstream
-            
-            echo "<h2>$question</h2>";
-            echo "<table border='1'>";
-            echo "<tr><th>$option1</th><th>$option2</th><th>$option3</th><th>$option4</th></tr>";
-=======
             // Display question and options in a table row
             echo "<tr><td colspan='2'><center><h2>$question</h2></center></td></tr>";
->>>>>>> Stashed changes
 
-        
+            // Display counts for each option
             $countRow = $countResult->fetch_assoc();
 
-<<<<<<< Updated upstream
-            
-            echo "<canvas id='chart$qid' width='200' height='200'></canvas>";
-=======
             // Create a canvas for the pie chart with reduced size
             // Create a canvas for the pie chart with reduced size
             echo "<tr><td><center><canvas id='chart$qid' width='200' height='200'></canvas></center></td></tr>";
->>>>>>> Stashed changes
             echo "<script>
                     var ctx = document.getElementById('chart$qid').getContext('2d');
                     var myChart = new Chart(ctx, {
@@ -172,6 +159,7 @@ if ($result->num_rows > 0) {
     echo "<tr><td colspan='2'>No questions available.</td></tr>";
 }
 
+// Close connection
 $conn->close();
 ?>
 
